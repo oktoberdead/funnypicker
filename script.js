a3fa441426ee68a9909bba4ee4c07a0706d50788
h <78501382+oktoberdead@users.noreply.github.com>
2023-03-31 16:45:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+let colorBox = document.getElementById("colorBox");
+let inputField = document.getElementById("inputField");
+colorBox.style.backgroundColor = "red";
 
+let formattedHEX;
+let unformattedHEX;
+
+inputField.oninput = function(){
+    
+    
+    if(this.value.length == 6) {
+        unformattedHEX = this.value;
+        for(let i = 0; i < 6; i++){
+            formattedHEX[i] = unformattedHEX[i] % 26;
+        }
+        colorBox.style.backgroundColor = "#" + formattedHEX;
+     }
+}
