f20ff8b093495f2f8bbdd1197596a52e9e9724fa
h <78501382+oktoberdead@users.noreply.github.com>
2023-03-31 22:25:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ let unformattedHEX;
 inputField.oninput = function(){
     
     
-    if(this.value.length == 6) {
+    if(inputField.value.length == 6) {
     console.log("More than 6");
-        unformattedHEX = this.value;
+        unformattedHEX = inputField.value;
         for(let i = 0; i < 6; i++){
             formattedHEX.charAt(i) = unformattedHEX.charAt(i) % 26;
             console.log("Changed " + unformattedHEX.charAt(i) + " to " + formattedHEX.charAt(i));
