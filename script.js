2bd5473a17936e0342575858b1c44401bbad9207
h <78501382+oktoberdead@users.noreply.github.com>
2023-03-31 20:23:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,9 +9,11 @@ inputField.oninput = function(){
     
     
     if(this.value.length == 6) {
+    console.log("More than 6");
         unformattedHEX = this.value;
         for(let i = 0; i < 6; i++){
             formattedHEX.charAt(i) = unformattedHEX.charAt(i) % 26;
+            console.log("Changed " + unformattedHEX.charAt(i) + " to " + formattedHEX.charAt(i));
         }
         colorBox.style.backgroundColor = "#" + formattedHEX;
         alert(1)
